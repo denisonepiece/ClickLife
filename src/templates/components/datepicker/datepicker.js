489ae0dfe81dab85e0e9db6041159ae984836576
denisonepiece <denisloverap@icloud.com>
2019-10-05 13:37:03 +0700

    Обновил datepicker v2

--- src/templates/components/datepicker/datepicker.js
+++ src/templates/components/datepicker/datepicker.js
@@ -1,13 +1,18 @@
 import 'air-datepicker'
 import './datepicker.css';
-import './datepicker.sass'
+import './datepicker.sass';
 
 $(".datepicker-field").datepicker({
   timepicker: true,
-  // inline: true,
   prevHtml: '<span class="datepicker__nav-prev"></span>',
   nextHtml: '<span class="datepicker__nav-next"></span>',
   clearButton: true,
+  onShow: function (inst) {
+    inst.el.classList.add('datepicker_is-active');
+  },
+  onHide: function (inst) {
+    inst.el.classList.remove('datepicker_is-active');
+  }
 });
 
 
