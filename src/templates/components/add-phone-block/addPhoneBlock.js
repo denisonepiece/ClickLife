3f94d354adb6eebc987aef91c21b49a001944e35
denisonepiece <denisloverap@icloud.com>
2019-10-11 16:50:13 +0700

    Обработка логики добавления номера телефона посредством js'a. Нужно отрефракторить.

---
+++ src/templates/components/add-phone-block/addPhoneBlock.js
@@ -0,0 +1,65 @@
+"use strict";
+
+import './add-phone-block.sass'
+import '../input-phone/input-phone'
+import Inputmask from 'inputmask';
+
+addPhoneBlock();
+
+function addPhoneBlock() {
+  const PhonesBlock = document.querySelector('.phones-block');
+  const addPhoneBtn = PhonesBlock.querySelector('.add-phone-button__button');
+  const inputBlock = PhonesBlock.querySelector('.input-phone');
+
+
+  function onFocusInputs(block) {
+    const inputs = block.querySelectorAll('input[type="tel"]');
+
+    for(let i = 0; i < inputs.length; i++) {
+      inputs[i].addEventListener('focus', function () {
+        inputs[i].parentNode.parentNode.classList.add('-input-focus-');
+      });
+
+      inputs[i].addEventListener('focusout', function () {
+        inputs[i].parentNode.parentNode.classList.remove('-input-focus-');
+      });
+    }
+  }
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    if(howElementsOnPage('.input-phone') === 3) {
+      addPhoneBtn.parentNode.remove();
+    }
+
+    onFocusInputs(PhonesBlock);
+  });
+
+  addPhoneBtn.addEventListener('click', function () {
+    //Клонирование и сброс значений
+    const inputClone = inputBlock.cloneNode(true);
+    const checkboxes = inputClone.querySelectorAll('input[type="checkbox"]');
+    inputClone.querySelector('.input__field').value = '';
+    for( let i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].checked = false;
+    }
+
+    addPhoneBtn.parentNode.before(inputClone);
+    inputClone.focus();
+    Inputmask({
+      "mask": "+7 (999) 999-99-99",
+    }).mask('input[type="tel"]');
+
+    onFocusInputs(PhonesBlock);
+    if(howElementsOnPage('.input-phone') === 3) {
+      addPhoneBtn.parentNode.remove();
+    }
+  });
+}
+
+function howElementsOnPage(selector) {
+  const elements = document.querySelectorAll(selector);
+
+  return elements.length;
+}
+
--- src/templates/components/input-phone/input-phone.js
+++ src/templates/components/input-phone/input-phone.js
@@ -1,2 +1,22 @@
-import './input-phone.sass'
+"use strict";
+
+import './input-phone.sass';
+import Inputmask from 'inputmask';
+
+Inputmask({"mask": "+7 (999) 999-99-99"}).mask('input[type="tel"]');
+
+const input = document.querySelector('input[type="tel"]');
+
+function validateInputPhone(input) {
+  const codes = [3, 4, 5, 6, 8, 9];
+  codes.includes(+input.value[4]);
+
+  input.addEventListener('keyup', function () {
+      if(input.value[input.value.length - 1] !== '_') {
+
+      }
+  });
+}
+
+validateInputPhone(input);
 
