fff6498bb04651cba2b32e7e5a52f3eb6fc5d15d
denisonepiece <denisloverap@icloud.com>
2019-10-17 18:34:07 +0700

    Добавил все виды уведомлений, разработал блок для уведомлений, и написал логику открытия и закрытия блока уведомлений

--- src/templates/components/notification/notification.js
+++ src/templates/components/notification/notification.js
@@ -1,1 +1,18 @@
 import './notification.sass'
+
+initAllNotification();
+
+function onclickNotification(notification) {
+  const links = notification.querySelectorAll('a');
+
+  notification.addEventListener('click', function () {
+    document.location.href = links[links.length - 1].getAttribute('href');
+  });
+}
+
+function initAllNotification() {
+  const Notices = document.querySelectorAll('.notification');
+
+  Notices.forEach((notice) => onclickNotification(notice));
+}
+
--- src/templates/includes/header/header.js
+++ src/templates/includes/header/header.js
@@ -1,1 +1,11 @@
 import './header.sass'
+
+
+function onClickNotice() {
+  const notice = document.querySelector('.notice');
+
+  notice.onclick = () => notice.classList.toggle('-is-open-');
+
+}
+
+onClickNotice();
