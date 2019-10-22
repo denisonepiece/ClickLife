c08cc2a56d708dd1b03941b4a24a6d0b1977bb0b
denisonepiece <denisloverap@icloud.com>
2019-10-22 20:31:44 +0700

    Добавил закрытие окна уведомления кликом мимо

--- src/templates/includes/header/header.js
+++ src/templates/includes/header/header.js
@@ -1,10 +1,21 @@
-import './header.sass'
+'use strict';
 
+import './header.sass'
 
 function onClickNotice() {
   const notice = document.querySelector('.notice');
 
   notice.onclick = () => notice.classList.toggle('-is-open-');
+
+  document.addEventListener('click', function (e) {
+    let container = $('.notice');
+
+    console.log(container.has(e.target).length);
+
+    if (container.has(e.target).length === 0) {
+      notice.classList.remove('-is-open-');
+    }
+  });
 
 }
 
