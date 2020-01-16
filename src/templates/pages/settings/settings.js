e6f4dc2a11ec59cc15d199a251b9fd85bcd8c870
denisonepiece <denisloverap@icloud.com>
2020-01-16 19:54:49 +0700

    Исправил подгрузку подкатегорий

--- src/templates/pages/settings/settings.js
+++ src/templates/pages/settings/settings.js
@@ -1,46 +1,30 @@
 import 'normalize.css';
-import '../../../sass/main.sass'
-import '../../../sass/typography.sass'
-import './settings.sass'
-//Includes
-import '../../includes/header/header'
-import '../../includes/footer/footer'
-//Components
-import '../../components/notification/notification'
-import '../../components/button/button'
-import '../../components/input/input'
-import '../../components/input-phone/input-phone'
+import '../../../sass/main.sass';
+import '../../../sass/typography.sass';
+import './settings.sass';
+// Includes
+import '../../includes/header/header';
+import '../../includes/footer/footer';
+// Components
+import '../../components/notification/notification';
+import '../../components/button/button';
+import '../../components/input/input';
+import '../../components/input-phone/input-phone';
 
-//Media
-import '../../../sass/media.sass'
+// Media
+import '../../../sass/media.sass';
 
 manageEmail();
 
 function manageEmail() {
-  const emailBlock = document.querySelector('.email');
-  const changeBtn = emailBlock.querySelector('.button');
-  const staticEmail = emailBlock.querySelector('.input_fixed');
-  const message = emailBlock.querySelector('.message');
-  const input = emailBlock.querySelector('.input__field');
+  const confrimed = document.querySelector('.email-confrimed');
+  const without = document.querySelector('.email-without');
+  const input = document.querySelector('.email-input');
+  const confrim = document.querySelector('.email-confirm');
 
 
-
-
-  changeBtn.onclick = () => {
-    let emailState = emailBlock.classList[1];
-
-    if(emailState === '-no-mail-') {
-      message.style.display = 'none';
-      input.parentNode.style.display = "block"
-    }
-
-    console.log();
-
-    if(input.parentNode.classList[1] == 'input_success') {
-      input.parentNode.style.display = "none";
-      message.style.display = 'block';
-    }
-
-  }
-
+  confrimed.querySelector('.button').addEventListener('click', function() {
+    confrimed.style.display = "none";
+    input.style.display = "block";
+  });
 }
--- src/templates/pages/task-create/task-create.js
+++ src/templates/pages/task-create/task-create.js
@@ -279,3 +279,83 @@ function changeCategories() {
   }
 }
 
+function initSelects(selectClass) {
+  let x;
+  let i;
+  let j;
+  let selElmnt;
+  let a;
+  let b;
+  let c;
+
+  x = document.querySelectorAll(selectClass);
+
+  for (i = 0; i < x.length; i++) {
+    selElmnt = x[i].getElementsByTagName('select')[0];
+    a = document.createElement('DIV');
+    a.setAttribute('class', 'input__field select-selected');
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    b = document.createElement('DIV');
+    b.setAttribute('class', 'select-items select-hide');
+    for (j = 1; j < selElmnt.length; j++) {
+      c = document.createElement('DIV');
+      c.innerHTML = selElmnt.options[j].innerHTML;
+      c.addEventListener('click', function (e) {
+        let y;
+        let i;
+        let k;
+        let s;
+        let h;
+        s = this.parentNode.parentNode.getElementsByTagName('select')[0];
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < s.length; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName('same-as-selected');
+            for (k = 0; k < y.length; k++) {
+              y[k].removeAttribute('class');
+            }
+            this.setAttribute('class', 'same-as-selected');
+            break;
+          }
+        }
+        h.click();
+      });
+      b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener('click', function (e) {
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle('select-hide');
+      this.parentNode.classList.toggle('select-arrow-active');
+    });
+  }
+
+  function closeAllSelect(elmnt) {
+    let x;
+    let y;
+    let i;
+    const arrNo = [];
+    x = document.getElementsByClassName('select-items');
+    y = document.getElementsByClassName('select-selected');
+    for (i = 0; i < y.length; i++) {
+      if (elmnt == y[i]) {
+        arrNo.push(i);
+      } else {
+        y[i].classList.remove('select-arrow-active');
+      }
+    }
+    for (i = 0; i < x.length; i++) {
+      if (arrNo.indexOf(i)) {
+        x[i].parentNode.classList.remove('select-arrow-active');
+        x[i].classList.add('select-hide');
+      }
+    }
+  }
+
+  document.addEventListener('click', closeAllSelect);
+}
+
